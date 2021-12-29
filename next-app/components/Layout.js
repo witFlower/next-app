a7735c6ec38f3271fd8197f5f0952b4fcb4e276d
i-e <i-e@yukoyuko.co.jp>
2021-12-29 18:13:04 +0900

    refactor: modify wrong path

--- next-app/components/Layout.js
+++ next-app/components/Layout.js
@@ -1,4 +1,4 @@
-import styles from "../styles/layout";
+import styles from "../styles/layout.module.scss";
 
 export default function Layout({ children }) {
   return <div className={styles.container}>{children}</div>;
